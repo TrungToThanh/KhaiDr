1cc876835e2e6a3e55d1746dcfe1349239e7eb42
Trung <thanhtrungantin@gmail.com>
2025-02-17 20:12:51 +0700

    demo v1 build

--- product/next.config.ts
+++ product/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: [
       "images.unsplash.com",
@@ -12,6 +11,17 @@ const nextConfig: NextConfig = {
       "www.sulwhasoo.com",
       "nocohub-001-prod-app-attachments.s3.us-east-2.amazonaws.com",
     ],
+    deviceSizes: [320, 420, 768, 1024, 1200], // Hỗ trợ ảnh responsive
+    formats: ["image/avif", "image/webp"], // Ưu tiên định dạng ảnh tối ưu
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
   },
 };
 
